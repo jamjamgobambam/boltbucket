0102d127ccc23701e0c7da0a9d47cfcb95ad78a1
jamjamgobambam <jamila.khawaja85@gmail.com>
2023-03-11 08:59:32 -0500

    fixed wheels controller

--- server/controllers/wheels.js
+++ server/controllers/wheels.js
@@ -17,7 +17,8 @@ export const getWheelsById = async (req, res) => {
     try {
         const category = 'wheels'
         const wheelId = parseInt(req.params.wheelId)
-        const results = await pool.query('SELECT * FROM options WHERE category=$1 AND id=$1', [category, wheelId])
+        console.log(wheelId)
+        const results = await pool.query('SELECT * FROM options WHERE category=$1 AND id=$2', [category, wheelId])
         res.status(200).json(results.rows)
         console.log(results.rows)
     } catch (error) {
